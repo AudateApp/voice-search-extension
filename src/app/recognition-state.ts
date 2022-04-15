5bd4d03f8497489ef0e0ef89917af26472dfa22c
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-15 03:11:56 +0000

    Add MicState model

--- src/app/recognition-state.ts
+++
@@ -1,7 +0,0 @@
-export enum RecognitionState {
-    UNKNOWN = 0,
-    NOT_SUPPORTED, // The browser does not support speech recognition natively
-    PERMISSION_NOT_GRANTED, 
-    AVAILABLE,
-    RECOGNIZING,
-}
