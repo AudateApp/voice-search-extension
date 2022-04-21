97d46172fc68af5ffcb5745582127a76d69df63f
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-21 20:18:58 +0000

    Hydrate service for querying input devices

---
+++ src/app/services/input-device.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+/** Provide abstraction for audio input devices. */
+@Injectable({
+  providedIn: 'root',
+})
+export class InputDeviceService {
+
+  constructor() {
+  }
+
+  getInputDevices(): Promise<MediaDeviceInfo[]> {
+    return navigator.mediaDevices.enumerateDevices().then((devices) => {
+      // this.devices = devices.filter(d => d.kind === "audioinput");
+      // this.currentDevice = this.devices.find(d => d.deviceId == "default");
+      // console.log("audio devices", this.devices, this.currentDevice);
+      return devices;
+    });
+  }
+
+  getDefaultDevice(): Promise<MediaDeviceInfo> {
+    return navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const device = devices.find(d => d.deviceId == "default");
+      if(device) {
+        return device;
+      } else {
+        console.error("Default device not found in devices list: ", devices);
+        throw "Default device not found";
+      }
+    });
+  }
+}
