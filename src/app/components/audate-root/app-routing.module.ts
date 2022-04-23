fb5d03ca8668031208ff6a6bdb1b2705d7b7c85b
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-23 00:02:30 +0000

    Add routes for specific components of the app

--- src/app/components/audate-root/app-routing.module.ts
+++ src/app/components/audate-root/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputPlateComponent } from '../input-plate/input-plate.component';
 import { PopupComponent } from '../popup/popup.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
+    path: 'popup',
+    component: PopupComponent,
+  },
+  {
+    path: 'input',
+    component: InputPlateComponent,
+  },
+  {
     path: '',
     component: AppComponent,
     pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: '',
   },
 ];
 
