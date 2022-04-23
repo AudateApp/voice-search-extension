4b669c2a0dbe08590f60086bf60e01648b55445d
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-23 01:26:00 +0000

    Wire up invocationType in root compoent and html files

--- src/app/components/audate-root/app.component.ts
+++ src/app/components/audate-root/app.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  isDevMode,
+  OnChanges,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { InvocationType } from 'src/app/model/invocation-type';
 
@@ -11,12 +20,65 @@ import { InvocationType } from 'src/app/model/invocation-type';
 export class AppComponent implements OnInit {
   title = 'audate';
   display: boolean = true;
-  @Input() invocationType: InvocationType = InvocationType.UNKNOWN;
+  @Input() invocationType: InvocationType;
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private router: Router,
+    private elementRef: ElementRef,
+    private primengConfig: PrimeNGConfig
+  ) {
+    this.invocationType =
+      elementRef.nativeElement.getAttribute('invocationType');
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+    switch (this.invocationType) {
+      case InvocationType.BACKGROUND_PAGE:
+        this.router.navigate(['background']).then(
+          (result) => {
+            if (!result) {
+              console.error('Failed to navigate to background page');
+            }
+          },
+          (errorReason) => console.error(errorReason)
+        );
+        break;
+      case InvocationType.OPTIONS_PAGE:
+        this.router.navigate(['options']).then(
+          (result) => {
+            if (!result) {
+              console.error('Failed to navigate to options page');
+            }
+          },
+          (errorReason) => console.error(errorReason)
+        );
+        break;
+      case InvocationType.BROWSER_ACTION:
+        this.router.navigate(['popup']).then(
+          (result) => {
+            if (!result) {
+              console.error('Failed to navigate to popup page');
+            }
+          },
+          (errorReason) => console.error(errorReason)
+        );
+        break;
+      default:
+        console.error('Unhandled invocation type ', this.invocationType);
+        if (this.invocationType == null && isDevMode()) {
+          console.error('TODO: Remove using popup for development');
+          this.router.navigate(['popup']).then(
+            (result) => {
+              if (!result) {
+                console.error('Failed to navigate to popup page');
+              }
+            },
+            (errorReason) => console.error(errorReason)
+          );
+        }
+        break;
+    }
   }
 
   showDialog() {
