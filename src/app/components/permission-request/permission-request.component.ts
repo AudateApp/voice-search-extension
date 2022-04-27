1a7eb92037201061decb15628c74c23ce899f6d4
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-27 21:44:39 +0100

    Hydrate permissions request

--- src/app/components/permission-request/permission-request.component.ts
+++ src/app/components/permission-request/permission-request.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'audate-permission-request',
   templateUrl: './permission-request.component.html',
-  styleUrls: ['./permission-request.component.scss']
+  styleUrls: ['./permission-request.component.scss'],
 })
-export class PermissionRequestComponent implements OnInit {
+export class PermissionRequestComponent {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  requestPermission() {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        (window as any).localStream = stream; // A
+        (window as any).localAudio.srcObject = stream; // B
+        (window as any).localAudio.autoplay = true; // C
+      })
+      .catch((err) => {
+        // Check error to see if dismissed or denied.
+        console.log('Permission not granted' + err);
+      });
   }
-
 }
