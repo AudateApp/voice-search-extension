3e1e39af269e9b1a0606fcd6ae9ba9c7986b384c
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-29 20:31:27 +0100

    Setup sentry tracing and error reporting

--- src/app/components/audate-root/app.module.ts
+++ src/app/components/audate-root/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { DialogModule } from 'primeng/dialog';
+import * as Sentry from '@sentry/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,10 +15,10 @@ import { InputPlateComponent } from '../input-plate/input-plate.component';
 import { TranscriptComponent } from '../transcript/transcript.component';
 import { AudioWavesComponent } from '../audio-waves/audio-waves.component';
 import { QuickSettingsComponent } from '../quick-settings/quick-settings.component';
-import { FormsModule } from '@angular/forms';
 import { OptionsPageComponent } from '../options-page/options-page.component';
 import { ContentPopupComponent } from '../content-popup/content-popup.component';
 import { PermissionRequestComponent } from '../permission-request/permission-request.component';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -39,7 +41,23 @@ import { PermissionRequestComponent } from '../permission-request/permission-req
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(trace: Sentry.TraceService) {
+    // myUndefinedFunction();
+  }
+}
