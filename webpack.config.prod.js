223d3c0ab89b2b53d921f226bd21bd639f5dea14
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-04-30 01:28:16 +0100

    Add webpack config for background and content scripts

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+const { join } = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    content_script: join(__dirname, 'src/chrome/content-script.ts'),
+    service_worker: join(__dirname, 'src/chrome/service-worker.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [{
+            loader: 'ts-loader',
+            options: {
+                configFile: "tsconfig.chrome.json"
+            }
+        }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: join(__dirname, './dist/audate'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
