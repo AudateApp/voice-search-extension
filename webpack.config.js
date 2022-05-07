0c3c135ce00ed6af4a271335cb6c8b0eda6994ad
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-05-07 02:21:39 +0100

    Use environment file in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,54 @@
-const { join } = require('path');
+// Run using `webpack --config webpack.config.js --env production`.
+const { join } = require("path");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    content_script: join(__dirname, 'src/chrome/content-script.ts'),
-    service_worker: join(__dirname, 'src/chrome/service-worker.ts')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: [{
-            loader: 'ts-loader',
-            options: {
-                configFile: "tsconfig.chrome.json"
-            }
-        }],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  output: {
-    path: join(__dirname, './dist/audate'),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
+module.exports = (env) => {
+  let mode = "development";
+  let envFilePath = "../environments/environment.ts";
+  if (env.production) {
+    mode = "production";
+    envFilePath = "../environments/environment.prod.ts";
   }
+
+  const config = {
+    mode: mode,
+    entry: {
+      content_script: join(__dirname, "src/chrome/content-script.ts"),
+      service_worker: join(__dirname, "src/chrome/service-worker.ts"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                configFile: "tsconfig.chrome.json",
+              },
+            },
+          ],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    output: {
+      path: join(__dirname, "./dist/audate"),
+      filename: "[name].js",
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    plugins: [
+      new webpack.NormalModuleReplacementPlugin(
+        /src\/environments\/environment\.ts/,
+        envFilePath
+      ),
+    ],
+  };
+
+  if (!env.production) {
+    config.devtool = "inline-source-map";
+  }
+  return config;
 };
