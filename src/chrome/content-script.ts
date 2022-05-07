b5f95d0e583dd1db319fad17a2ce47ed3874b631
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-05-07 15:42:20 +0100

    Insert popup in page via content-script

--- src/chrome/content-script.ts
+++ src/chrome/content-script.ts
@@ -1,1 +1,15 @@
-console.log('hello content-script');
+import { environment } from '../environments/environment';
+
+function insertPopup() {
+  console.debug('inserting audate popup');
+  const audateWrapper = document.createElement('div');
+  audateWrapper.className = 'audate-wrapper';
+  const shadow = audateWrapper.attachShadow({ mode: 'open' });
+  shadow.innerHTML = `
+    <iframe 
+        part="audate-frame" 
+        src="chrome-extension://${environment.extensionId}/index.html#content-popup"
+    ></iframe>`;
+  document.body.appendChild(audateWrapper);
+}
+insertPopup();
