a8dfd7c6d6be13c1ae00b476bae65dc85c608825
Justice Ogbonna <justiceo@users.noreply.github.com>
2022-07-19 04:37:14 -0700

    Add UI event handling to dialog alongside responsiveness

--- src/app/page-loader/page-loader.component.ts
+++ src/app/page-loader/page-loader.component.ts
@@ -9,4 +9,22 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class PageLoaderComponent {
   constructor(private primengConfig: PrimeNGConfig) {}
+  onResizeStart(e: any) {
+    console.error('resize start', e);
+  }
+  onResizeEnd(e: any) {
+    console.error('resize end', e);
+  }
+  onShow(e: any) {
+    console.error('on show: ', e);
+  }
+  onHide(e: any) {
+    console.error('on hide: ', e);
+  }
+  onDragEnd(e: any) {
+    console.error('on drag end: ', e);
+  }
+  onMaximize(e: any) {
+    console.error('on max: ', e);
+  }
 }
