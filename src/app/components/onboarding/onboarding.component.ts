b97c859122abadcae56a7677e7b6fef6673b3a2f
Justice Ogbonna <ejo36@drexel.edu>
2022-10-14 22:05:05 +0100

    Add basic styling for onboarding screen

--- src/app/components/onboarding/onboarding.component.ts
+++ src/app/components/onboarding/onboarding.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Logger } from 'src/app/services/logging/logger';
+import { LoggingService } from 'src/app/services/logging/logging.service';
 import { applyConfig, DefaultConfig } from '../audio-waves/audio-wave';
 
 @Component({
@@ -6,16 +8,20 @@ import { applyConfig, DefaultConfig } from '../audio-waves/audio-wave';
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.scss'],
 })
-export class OnboardingComponent implements OnInit {
+export class OnboardingComponent {
   steps = ['hello', 'world'];
-
+  notice = "";
   headerWave = DefaultConfig;
   footerWave = DefaultConfig;
+  logger: Logger;
 
-  constructor() {
+  constructor(loggingService: LoggingService) {
+    this.logger = loggingService.getLogger("onboarding");
+
     this.headerWave = applyConfig({
       width: 0,
-      height: 200,
+      height: 30,
+      nodeCount: 70,
       opaqueColor: '#410ff8',
     });
 
@@ -28,5 +34,38 @@ export class OnboardingComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  requestPermission() {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        stream.getTracks().forEach((track) => {
+          track.stop();
+          // Wait a second then close the tab.
+          setTimeout(() => {
+            window.close();
+          }, 2000);
+        });
+        this.notice = 'Permission Granted';
+      })
+      .catch((err) => {
+        /*
+         * TODO: Handle errors:
+         *
+         * Chrome error messages.
+         * err: DOMException: Permission denied
+         * err: DOMException: Permission dismissed
+         * err: DOMException: Permission denied by system // browser doesn't have access
+         *
+         * Firefox:
+         * err: DOMException: The request is not allowed by the user agent or the platform in the current context.
+         * err: DOMException: The object can not be found here - (the browser doesn't have mic permission).
+         *
+         * Safari:
+         * err: NotAllowedError: The request is not allowed by the user agent or the platform in the current context, possibly because the user denied permission.
+         *
+         */
+        this.logger.error('Error requesting permission ', err);
+        this.notice = 'Permission not granted';
+      });
+  }
 }
