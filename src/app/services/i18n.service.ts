21a1c6dadf4ec89223dc5361f1f74f6037d33cdc
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-10-07 23:52:41 -0700

    Use en-US as fallback when chrome.i18n not available

--- src/app/services/i18n.service.ts
+++ src/app/services/i18n.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Logger } from './logging/logger';
 import { LoggingService } from './logging/logging.service';
+import * as enMessage from '../../_locales/en/messages.json';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +18,18 @@ export class I18nService {
     if (chrome?.i18n) {
       return chrome.i18n.getMessage(key);
     }
-    this.logger.error('chrome.i18n is not available in the current context');
+
+    this.logger.warn(
+      'chrome.i18n is not available in the current context, trying en-US as fallback'
+    );
+    Object.keys(enMessage).forEach((k) => {});
+    for (const [translationKey, translatedText] of Object.entries(enMessage)) {
+      if (translationKey === key) {
+        return translatedText['message'];
+      }
+    }
+
+    this.logger.error('No translation available for key', key);
     return key;
   }
 }
